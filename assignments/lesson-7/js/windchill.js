202da3802d99fec3b4fe76a730dfcf9948e9d003
jeraldvj <jeraldvj@byui.edu>
2019-06-07 01:08:20 -0400

    CSS Flexbox & Grid

---
+++ assignments/lesson-7/js/windchill.js
@@ -0,0 +1,12 @@
+
+    var t = parseInt(document.getElementById("temperature").innerHTML);
+    var s = parseInt(document.getElementById("WindSpeed").innerHTML);
+    var ex = Math.pow(s,0.16);
+    var w = 35.74+0.6215*t-35.75*ex +0.4275*t*ex;
+    w = Math.round(w);    
+
+    //var dig = 2;
+    //var multi = Math.pow(10,dig);
+    //w = Math.round(w*multi) / multi;
+
+    document.getElementById("WindChill").innerHTML = w;

3941c79fc5daeaef499ea6774db100de2d8bfcb9
jeraldvj <jeraldvj@byui.edu>
2019-07-03 23:13:43 -0400

    all new

---
+++ assignments/lesson-11/js/windchill.js
@@ -0,0 +1,12 @@
+
+    var t = parseFloat(document.getElementById("temp").innerHTML);
+    var s = parseFloat(document.getElementById("windSpd").innerHTML);
+    var ex = Math.pow(s,0.16);
+    var w = 35.74+0.6215*t-35.75*ex +0.4275*t*ex;
+    w = Math.round(w);    
+
+    //var dig = 2;
+    //var multi = Math.pow(10,dig);
+    //w = Math.round(w*multi) / multi;
+
+    document.getElementById("windCh").innerHTML = w;
